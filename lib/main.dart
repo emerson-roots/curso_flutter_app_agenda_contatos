a260f2e10f44e34dbeb4eb2fabd61d547cc242f2
Emerson Melo <emerson_sardinha@hotmail.com>
2023-02-14 14:42:04 -0300

    Aula 172 - Criando a tela de contato

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:agenda_de_contatos/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
+import 'ui/contact_page.dart';
+
 void main() {
   runApp(MaterialApp(
-    home: HomePage()
+    home: ContactPage(),
+    debugShowCheckedModeBanner: false,
   ));
 }
